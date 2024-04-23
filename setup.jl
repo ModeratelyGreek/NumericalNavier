79f76ff6d40de87d9beee38892157b2ee0ef5b36
superlion3 <melvinchen610@gmail.com>
2024-04-23 07:53:16 -0400

    guided myself through and set up file structure, what is going on

---
+++ setup.jl
@@ -0,0 +1,13 @@
+# You don't need to run this.
+
+using Pkg
+Pkg.activate(".")
+Pkg.add("IJulia")
+Pkg.add("LaTeXStrings")
+Pkg.add("FileIO")
+Pkg.add("ImageIO")
+Pkg.add("Images")
+Pkg.add("Plots")
+Pkg.add("Base64")
+Pkg.add("ImageMagick")
+Pkg.add("Statistics")
